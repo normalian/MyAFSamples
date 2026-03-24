8d48a93f448f9db4b3ada87da490744b32bca7a8
Daichi Isami <warito_hutsu@hotmail.com>
2026-03-24 00:20:18 -0700

    add Sample11FoundryBasicConsoleApp

---
+++ Sample11FoundryBasicConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+class Program
+{
+    static async Task Main()
+    {
+        // Foundry endpoint
+        var endpoint = Environment.GetEnvironmentVariable("MS_FOUNDRY_ENDPOINT") ?? throw new InvalidOperationException("MS_FOUNDRY_ENDPOINT is not set.");
+        var agentname = "your-agent-name";
+        var version = "1";
+        var prompt = "Please introduce yourself.";
+
+        AIProjectClient aiProjectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+        AgentVersion agentVersion = aiProjectClient.Agents.GetAgentVersion(agentname, version);
+
+        // You can use an AIAgent with an already created server side agent version.
+        AIAgent agent = aiProjectClient.AsAIAgent(agentVersion);
+        var options = new AgentRunOptions();
+        AgentResponse response = await agent.RunAsync(prompt, null, options);
+        Console.WriteLine(response.Text);
+    }
+}

65ea8fbdd6008a209c4b75e86598dd87bc206093
Daichi Isami <warito_hutsu@hotmail.com>
2026-04-02 23:39:34 -0700

    update sample08 for MCP server

--- Sample08MCPServerFunctionApp/Program.cs
+++ Sample08MCPServerFunctionApp/Program.cs
@@ -1,10 +1,11 @@
 using Azure.AI.OpenAI;
 using Azure.Identity;
 using Microsoft.Agents.AI;
-using Microsoft.Agents.AI.DurableTask;
-using Microsoft.Agents.AI.Hosting.AzureFunctions;
-using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.AI;
+using Microsoft.Agents.AI.Hosting.AzureFunctions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 class Program
@@ -13,7 +14,17 @@ class Program
     {
         var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
         var deploymentName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-4.1-mini";
-        AzureOpenAIClient client = new AzureOpenAIClient(new Uri(endpoint), new AzureCliCredential());
+        AzureOpenAIClient client = new AzureOpenAIClient(new Uri(endpoint), new DefaultAzureCredential());
+
+        // Put following mcp.json file in the root of your function app to enable MCP server. The url should be your function app url plus "/runtime/webhooks/mcp/sse".
+        // {
+        //        "servers": {
+        //        "mine-mcp-server-local": {
+        //          "type": "sse",
+        //              "url": "https://your-appservice-name.azurewebsites.net/runtime/webhooks/mcp/sse"
+        //         }
+        //    }
+        // }
 
         AIAgent agent = client.GetChatClient(deploymentName).AsIChatClient().AsAIAgent(
             instructions: @"In this Normalian project, to facilitate management, class names must follow naming conventions based on business categories.
@@ -21,7 +32,7 @@ class Program
             Please define classes using the names provided here. Do not define classes with any other names.
 
             If you are unsure about the business category from the class name, use the `DetermineBusinessCategory` tool to obtain the business category.",
-            name: "NormalianProjectAdvisor",
+            name: "NormalianMyProjectAdvisor",
             description: "This tool describes naming rule overview of the Normalian project.");
 
         using IHost app = FunctionsApplication
